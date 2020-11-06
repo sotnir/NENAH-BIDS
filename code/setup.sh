0630eb09676ff7d8cf614e506ba6cb8db7982c0b
Finn Lennartsson <finn.lennartsson@gmail.com>
2020-11-06 09:04:24 +0100

    Removed /code folder from repository

--- code/setup.sh
+++
@@ -1,9 +0,0 @@
-## Export the path to the download data
-
-# This gobblegook comes from stack overflow as a means to find the directory containing the current function: https://stackoverflow.com/a/246128
-CODE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export CODE_DIR
-
-echo BIDS codeFolder is $CODE_DIR
-
-# exit 0
