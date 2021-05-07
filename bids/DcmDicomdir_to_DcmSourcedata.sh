0ff9c25f2dc4ba5beaf3f68596823a244fb09b93
finnlennartsson <finn.lennartsson@gmail.com>
2021-05-07 11:17:33 +0200

    Update DcmDicomdir_to_DcmSourcedata.sh

--- bids/DcmDicomdir_to_DcmSourcedata.sh
+++ bids/DcmDicomdir_to_DcmSourcedata.sh
@@ -1,36 +1,56 @@
 #!/bin/bash
-#
 ## NENAH study
 #
-## PREPROCESS DATA, including:
-#   1. arrangement of DICOMs into organised folders in /sourcedata folder
-#
-# Is run from BIDS-folder 
-# Input
-# $1 = subject_id (e.g. P4, NENAH007)
+usage()
+{
+  base=$(basename "$0")
+  echo "usage: $base sID [options]
+Arrangement of DICOMs into organised folders in /sourcedata folderConversion of DICOMs to BIDS and validation of BIDS dataset
 
-# Exit upon any error
-set -exo pipefail
+Arguments:
+  sID				Subject ID (e.g. NENAHC001) 
+Options:
+  -h / -help / --help           Print usage.
+"
+  exit;
+}
 
-## To make codeFolder a global variable 
+################ ARGUMENTS ################
+
+[ $# -ge 1 ] || { usage; }
+command=$@
+sID=$1
+
+# Define Folders
 # This gobblegook comes from stack overflow as a means to find the directory containing the current function: https://stackoverflow.com/a/246128
-codeFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Study/subject specific #
-studyFolder=`pwd`;
-origdcmFolder=$studyFolder/dicomdir;
-dcmFolder=$studyFolder/sourcedata
+codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+studydir=`pwd`
+origdcmdir=$studydir/dicomdir;
+dcmdir=$studydir/sourcedata
+scriptname=`basename $0 .sh`
 
 # Arguments
 sID=$1
-logFolder=${studyFolder}/derivatives/preprocessing_logs/sub-${sID}
+logdir=${studydir}/derivatives/preprocessing_logs/sub-${sID}
+
+shift
+while [ $# -gt 0 ]; do
+    case "$1" in
+	-h|-help|--help) usage; ;;
+	-*) echo "$0: Unrecognized option $1" >&2; usage; ;;
+	*) break ;;
+    esac
+    shift
+done
 
 if [ ! -d $logFolder ]; then mkdir -p $logFolder; fi
+
+### Start processing ###
 
 # Re-arrange DICOMs into sourcedata
 if [ ! -d $dcmFolder ]; then mkdir $dcmFolder; fi
 dcm2niix -b o -r y -w 1 -o $dcmFolder -f sub-$sID/s%2s_%d/%d_%5r.dcm $origdcmFolder/${sID}
 
 # Simple log
-echo "Executing $0 $@ "> ${logFolder}/sub-${sID}_dcm2sourcedata.log 2>&1 
-cat $0 >> ${logFolder}/sub-${sID}_dcm2sourcedata.log 2>&1 
+echo "Executing $0 $@ "> ${logdir/sub-${sID}_dcm2sourcedata.log 2>&1 
+cat $0 >> ${logdir}/sub-${sID}_dcm2sourcedata.log 2>&1 
