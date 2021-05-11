7a3e07059429478826b80ed945e9217a7b0cfaf7
finnlennartsson <finn.lennartsson@gmail.com>
2021-05-11 11:59:36 +0200

    Update DcmDicomdir_to_DcmSourcedata.sh

--- bids/DcmDicomdir_to_DcmSourcedata.sh
+++ bids/DcmDicomdir_to_DcmSourcedata.sh
@@ -49,6 +49,5 @@ cat $0 >> ${logdir}/sub-${sID}_$scriptname.log 2>&1
 
 # Re-arrange DICOMs into sourcedata
 if [ ! -d $dcmFolder ]; then mkdir $dcmFolder; fi
-dcm2niix -b o -r y -w 1 -o $dcmFolder -f sub-$sID/s%2s_%d/%d_%5r.dcm $origdcmFolder/${sID} \
->> ${logdir}/sub-${sID}_$scriptname.log 2>&1
+dcm2niix -b o -r y -w 1 -o $dcmFolder -f sub-$sID/s%2s_%d/%d_%5r.dcm $origdcmFolder/${sID}
 
