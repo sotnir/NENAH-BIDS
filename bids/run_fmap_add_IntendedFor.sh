5344a58aeeb2a0528c27aa6dd240dead41441b5d
Yukai Zou <yukaizou2012@gmail.com>
2021-10-05 11:48:01 +0100

    Commented out MRIQC in DcmSourcedata_to_NiftiRawdata.sh for testing

--- bids/run_fmap_add_IntendedFor.sh
+++ bids/run_fmap_add_IntendedFor.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-python fmap_add_IntendedFor.py sub-NENAH002/fmap/sub-NENAH002_acq-gre_run-1_phasediff.json sub-NENAH002/func/sub-NENAH002_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH004/fmap/sub-NENAH004_acq-gre_run-1_phasediff.json sub-NENAH004/func/sub-NENAH004_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH005/fmap/sub-NENAH005_acq-gre_run-1_phasediff.json sub-NENAH005/func/sub-NENAH005_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH007/fmap/sub-NENAH007_acq-gre_run-1_phasediff.json sub-NENAH007/func/sub-NENAH007_task-rest_run-1_bold.nii.gz sub-NENAH007/func/sub-NENAH007_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH010/fmap/sub-NENAH010_acq-gre_run-1_phasediff.json sub-NENAH010/func/sub-NENAH010_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH011/fmap/sub-NENAH011_acq-gre_run-1_phasediff.json sub-NENAH011/func/sub-NENAH011_task-rest_run-1_bold.nii.gz sub-NENAH011/func/sub-NENAH011_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH012/fmap/sub-NENAH012_acq-gre_run-1_phasediff.json sub-NENAH012/func/sub-NENAH012_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH014/fmap/sub-NENAH014_acq-gre_run-1_phasediff.json sub-NENAH014/func/sub-NENAH014_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH015/fmap/sub-NENAH015_acq-gre_run-1_phasediff.json sub-NENAH015/func/sub-NENAH015_task-rest_run-1_bold.nii.gz sub-NENAH015/func/sub-NENAH015_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH016/fmap/sub-NENAH016_acq-gre_run-1_phasediff.json sub-NENAH016/func/sub-NENAH016_task-rest_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH002/fmap/sub-NENAH002_acq-gre_run-1_phasediff.json sub-NENAH002/func/sub-NENAH002_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH004/fmap/sub-NENAH004_acq-gre_run-1_phasediff.json sub-NENAH004/func/sub-NENAH004_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH005/fmap/sub-NENAH005_acq-gre_run-1_phasediff.json sub-NENAH005/func/sub-NENAH005_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH007/fmap/sub-NENAH007_acq-gre_run-1_phasediff.json sub-NENAH007/func/sub-NENAH007_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH007/func/sub-NENAH007_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH010/fmap/sub-NENAH010_acq-gre_run-1_phasediff.json sub-NENAH010/func/sub-NENAH010_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH011/fmap/sub-NENAH011_acq-gre_run-1_phasediff.json sub-NENAH011/func/sub-NENAH011_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH011/func/sub-NENAH011_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH012/fmap/sub-NENAH012_acq-gre_run-1_phasediff.json sub-NENAH012/func/sub-NENAH012_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH014/fmap/sub-NENAH014_acq-gre_run-1_phasediff.json sub-NENAH014/func/sub-NENAH014_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH015/fmap/sub-NENAH015_acq-gre_run-1_phasediff.json sub-NENAH015/func/sub-NENAH015_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH015/func/sub-NENAH015_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH016/fmap/sub-NENAH016_acq-gre_run-1_phasediff.json sub-NENAH016/func/sub-NENAH016_task-rest_dir-AP_run-1_bold.nii.gz
 # sub-NENAH024
-python fmap_add_IntendedFor.py sub-NENAH024/fmap/sub-NENAH024_acq-gre_run-1_phasediff.json sub-NENAH024/func/sub-NENAH024_task-rest_run-1_bold.nii.gz sub-NENAH024/func/sub-NENAH024_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH031/fmap/sub-NENAH031_acq-gre_run-1_phasediff.json sub-NENAH031/func/sub-NENAH031_task-rest_run-1_bold.nii.gz sub-NENAH031/func/sub-NENAH031_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAH033/fmap/sub-NENAH033_acq-gre_run-1_phasediff.json sub-NENAH033/func/sub-NENAH033_task-rest_run-1_bold.nii.gz sub-NENAH033/func/sub-NENAH033_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC001/fmap/sub-NENAHC001_acq-gre_run-1_phasediff.json sub-NENAHC001/func/sub-NENAHC001_task-rest_run-1_bold.nii.gz sub-NENAHC001/func/sub-NENAHC001_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC002/fmap/sub-NENAHC002_acq-gre_run-1_phasediff.json sub-NENAHC002/func/sub-NENAHC002_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC003/fmap/sub-NENAHC003_acq-gre_run-1_phasediff.json sub-NENAHC003/func/sub-NENAHC003_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC004/fmap/sub-NENAHC004_acq-gre_run-1_phasediff.json sub-NENAHC004/func/sub-NENAHC004_task-rest_run-1_bold.nii.gz sub-NENAHC004/func/sub-NENAHC004_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC005/fmap/sub-NENAHC005_acq-gre_run-2_phasediff.json sub-NENAHC005/func/sub-NENAHC005_task-rest_run-1_bold.nii.gz sub-NENAHC005/func/sub-NENAHC005_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC009/fmap/sub-NENAHC009_acq-gre_run-1_phasediff.json sub-NENAHC009/func/sub-NENAHC009_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC012/fmap/sub-NENAHC012_acq-gre_run-1_phasediff.json sub-NENAHC012/func/sub-NENAHC012_task-rest_run-1_bold.nii.gz sub-NENAHC012/func/sub-NENAHC012_task-rest_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH024/fmap/sub-NENAH024_acq-gre_run-1_phasediff.json sub-NENAH024/func/sub-NENAH024_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH024/func/sub-NENAH024_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH031/fmap/sub-NENAH031_acq-gre_run-1_phasediff.json sub-NENAH031/func/sub-NENAH031_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH031/func/sub-NENAH031_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAH033/fmap/sub-NENAH033_acq-gre_run-1_phasediff.json sub-NENAH033/func/sub-NENAH033_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAH033/func/sub-NENAH033_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC001/fmap/sub-NENAHC001_acq-gre_run-1_phasediff.json sub-NENAHC001/func/sub-NENAHC001_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC001/func/sub-NENAHC001_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC002/fmap/sub-NENAHC002_acq-gre_run-1_phasediff.json sub-NENAHC002/func/sub-NENAHC002_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC003/fmap/sub-NENAHC003_acq-gre_run-1_phasediff.json sub-NENAHC003/func/sub-NENAHC003_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC004/fmap/sub-NENAHC004_acq-gre_run-1_phasediff.json sub-NENAHC004/func/sub-NENAHC004_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC004/func/sub-NENAHC004_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC005/fmap/sub-NENAHC005_acq-gre_run-2_phasediff.json sub-NENAHC005/func/sub-NENAHC005_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC005/func/sub-NENAHC005_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC009/fmap/sub-NENAHC009_acq-gre_run-1_phasediff.json sub-NENAHC009/func/sub-NENAHC009_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC012/fmap/sub-NENAHC012_acq-gre_run-1_phasediff.json sub-NENAHC012/func/sub-NENAHC012_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC012/func/sub-NENAHC012_task-rest_dir-AP_run-2_bold.nii.gz
 # sub-NENAHC013
-python fmap_add_IntendedFor.py sub-NENAHC013/fmap/sub-NENAHC013_acq-gre_run-1_phasediff.json sub-NENAHC013/func/sub-NENAHC013_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC014/fmap/sub-NENAHC014_acq-gre_run-1_phasediff.json sub-NENAHC014/func/sub-NENAHC014_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC015/fmap/sub-NENAHC015_acq-gre_run-1_phasediff.json sub-NENAHC015/func/sub-NENAHC015_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC017/fmap/sub-NENAHC017_acq-gre_run-1_phasediff.json sub-NENAHC017/func/sub-NENAHC017_task-rest_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC013/fmap/sub-NENAHC013_acq-gre_run-1_phasediff.json sub-NENAHC013/func/sub-NENAHC013_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC014/fmap/sub-NENAHC014_acq-gre_run-1_phasediff.json sub-NENAHC014/func/sub-NENAHC014_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC015/fmap/sub-NENAHC015_acq-gre_run-1_phasediff.json sub-NENAHC015/func/sub-NENAHC015_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC017/fmap/sub-NENAHC017_acq-gre_run-1_phasediff.json sub-NENAHC017/func/sub-NENAHC017_task-rest_dir-AP_run-1_bold.nii.gz
 # sub-NENAHC018
-python fmap_add_IntendedFor.py sub-NENAHC018/fmap/sub-NENAHC018_acq-gre_run-1_phasediff.json sub-NENAHC018/func/sub-NENAHC018_task-rest_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC018/fmap/sub-NENAHC018_acq-gre_run-1_phasediff.json sub-NENAHC018/func/sub-NENAHC018_task-rest_dir-AP_run-1_bold.nii.gz
 # sub-NENAHC020
-python fmap_add_IntendedFor.py sub-NENAHC020/fmap/sub-NENAHC020_acq-gre_run-1_phasediff.json sub-NENAHC020/func/sub-NENAHC020_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC022/fmap/sub-NENAHC022_acq-gre_run-1_phasediff.json sub-NENAHC022/func/sub-NENAHC022_task-rest_run-1_bold.nii.gz sub-NENAHC022/func/sub-NENAHC022_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC023/fmap/sub-NENAHC023_acq-gre_run-1_phasediff.json sub-NENAHC023/func/sub-NENAHC023_task-rest_run-1_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC024/fmap/sub-NENAHC024_acq-gre_run-1_phasediff.json sub-NENAHC024/func/sub-NENAHC024_task-rest_run-1_bold.nii.gz sub-NENAHC024/func/sub-NENAHC024_task-rest_run-2_bold.nii.gz
-python fmap_add_IntendedFor.py sub-NENAHC026/fmap/sub-NENAHC026_acq-gre_run-1_phasediff.json sub-NENAHC026/func/sub-NENAHC026_task-rest_run-1_bold.nii.gz sub-NENAHC026/func/sub-NENAHC026_task-rest_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC020/fmap/sub-NENAHC020_acq-gre_run-1_phasediff.json sub-NENAHC020/func/sub-NENAHC020_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC022/fmap/sub-NENAHC022_acq-gre_run-1_phasediff.json sub-NENAHC022/func/sub-NENAHC022_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC022/func/sub-NENAHC022_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC023/fmap/sub-NENAHC023_acq-gre_run-1_phasediff.json sub-NENAHC023/func/sub-NENAHC023_task-rest_dir-AP_run-1_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC024/fmap/sub-NENAHC024_acq-gre_run-1_phasediff.json sub-NENAHC024/func/sub-NENAHC024_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC024/func/sub-NENAHC024_task-rest_dir-AP_run-2_bold.nii.gz
+python fmap_add_IntendedFor.py sub-NENAHC026/fmap/sub-NENAHC026_acq-gre_run-1_phasediff.json sub-NENAHC026/func/sub-NENAHC026_task-rest_dir-AP_run-1_bold.nii.gz sub-NENAHC026/func/sub-NENAHC026_task-rest_dir-AP_run-2_bold.nii.gz
