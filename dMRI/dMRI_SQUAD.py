a6666e13a7a7c6d35f02f449cf7bb9efdf866b72
finnlennartsson <finn.lennartsson@gmail.com>
2024-02-04 11:46:40 +0100

    Update dMRI_SQUAD.py

--- dMRI/dMRI_SQUAD.py
+++ dMRI/dMRI_SQUAD.py
@@ -1,5 +1,5 @@
 # Routine for running SQUAD in study
-# and saving to session_QC_SQUAD.tsv file
+# and saving to QC_SQUAD.tsv file
 
 def perform_process(processcall) :
     import os, subprocess
@@ -20,14 +20,13 @@ def perform_process(processcall) :
 import os, shutil, json
 from glob import glob
 
-# Define session and derivatives folder
-session = 'MR2'
-derivatives = '/home/perinedo/Projects/PK_PMR/derivatives/dMRI'
-# and list sub-sID/ses-ssID that have done quad
-quadfolders = glob(os.path.join(derivatives,'sub-PMR*',f'ses-{session}','qc/quad'))
+# Define derivatives folder
+derivatives = '/data/1TSSD/NENAH_BIDS/derivatives/dMRI'
+# and list sub-sID that have done quad
+quadfolders = glob(os.path.join(derivatives,'sub-NENAH*','qc/eddy_quad'))
 
-# Create squad folder in sub-GROUP/ses-{session}/qc/squad
-squadfolder = os.path.join(derivatives,f'sub-GROUP/ses-{session}/qc/squad')
+# Create squad folder in sub-GROUP/qc/squad
+squadfolder = os.path.join(derivatives,f'sub-GROUP/qc/eddy_squad')
 if not os.path.exists(squadfolder): # then make this directory
     os.makedirs(squadfolder)
 # and write the squadlist file to this (required as input to eddy_quad)
@@ -58,10 +57,11 @@ with open(os.path.join(squadfolder,'group_db.json'), 'r') as f:
   squad = json.load(f)
 # and put into dataframes
 df1 = pd.DataFrame(squad["qc_motion"], columns=['qc_motion_abs',  'qc_motion_rel'], dtype = float)
-df2 = pd.DataFrame(squad["qc_cnr"], columns=['qc_snr_b0',  'qc_cnr_b0400',  'qc_cnr_b1000',  'qc_cnr_b2500'], dtype = float)
+#df2 = pd.DataFrame(squad["qc_cnr"], columns=['qc_snr_b0',  'qc_cnr_b0400',  'qc_cnr_b1000',  'qc_cnr_b2500'], dtype = float)
 df3 = pd.DataFrame(squad["qc_outliers"], columns=['qc_outliers_tot', 'qc_outliers_b400', 'qc_outliers_b1000', 'qc_outliers_b2500','qc_outliers_pe'], dtype = float)
 # and a final dataframe
-df =  pd.concat([df1,df2, df3['qc_outliers_tot']], axis=1, join='outer')
+df =  pd.concat([df1, df3['qc_outliers_tot']], axis=1, join='outer')
+#df =  pd.concat([df1,df2, df3['qc_outliers_tot']], axis=1, join='outer')
 
 # Create dataframe for deciding QC
 dfqc = pd.DataFrame(np.zeros(df.shape)) # same shape as df but filled with zeros
@@ -75,22 +75,26 @@ dfqc[abs( df.mean(axis=0) - df ) < 1 * df.std(axis=0) ] = 1
 ## Write to output file
 # Get sID and ssID:s from quadfolders
 sID_ssID = [s.replace(derivatives+"/", "") for s in quadfolders]
-sID = [s.replace(f"/ses-{session}/qc/quad", "") for s in sID_ssID]
+sID = [s.replace("qc/eddy_quad", "") for s in sID_ssID]
 df_sID_ssID = pd.DataFrame(sID, columns=["participant_id"])
-df_sID_ssID["session_id"]=f"ses-{session}"
 
 # rename the columns
 dfqc.rename(columns = {'qc_motion_abs':'qc_motion_abs_pass_fail',
                        'qc_motion_rel':'qc_motion_rel_pass_fail',
-                       'qc_snr_b0':'qc_snr_b0_pass_fail',
-                       'qc_cnr_b0400':'qc_cnr_b0400_pass_fail',
-                       'qc_cnr_b1000':'qc_cnr_b1000_pass_fail',
-                       'qc_cnr_b2500':'qc_cnr_b2500_pass_fail',
                        'qc_outliers_tot_pass_fail':'qc_outliers_tot_pass_fail'}, 
                        inplace = True)
+#dfqc.rename(columns = {'qc_motion_abs':'qc_motion_abs_pass_fail',
+#                       'qc_motion_rel':'qc_motion_rel_pass_fail',
+#                       'qc_snr_b0':'qc_snr_b0_pass_fail',
+#                       'qc_cnr_b0400':'qc_cnr_b0400_pass_fail',
+#                       'qc_cnr_b1000':'qc_cnr_b1000_pass_fail',
+#                       'qc_cnr_b2500':'qc_cnr_b2500_pass_fail',
+#                       'qc_outliers_tot_pass_fail':'qc_outliers_tot_pass_fail'}, 
+#                       inplace = True)
+
 dfqc =  pd.concat([df_sID_ssID, dfqc], axis=1, join='outer')
 dfqc = dfqc.sort_values( by = 'participant_id')
 # and write to output-file
-squadqctsv = os.path.join(squadfolder,'session_QC_SQUAD.tsv')
+squadqctsv = os.path.join(squadfolder,'QC_SQUAD.tsv')
 dfqc.to_csv(squadqctsv, sep="\t", index=False)
 
