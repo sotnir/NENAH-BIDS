fe7ce77ce1d6f0fbe81f7cba4e68e50d98da47ae
finnlennartsson <finn.lennartsson@gmail.com>
2024-03-19 13:43:38 +0100

    Update dMRI_SQUAD_last_bit_only.py

--- dMRI/dMRI_SQUAD_last_bit_only.py
+++ dMRI/dMRI_SQUAD_last_bit_only.py
@@ -8,6 +8,7 @@ derivatives = '/data/1TSSD/NENAH_BIDS/derivatives/dMRI'
 squadfolder = os.path.join(derivatives,f'sub-NENAHGRP/qc/squad_with_grouping_exclNENAHC041')
 
 # Read SQUAD output (GROUP JSON-file)
+import os, shutil, json
 import json
 import numpy as np
 import pandas as pd
