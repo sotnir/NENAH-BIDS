44aa9b91ae4abc7931dfd5fdcd3cd889b2a925ad
Finn Lennartsson <finn.lennartsson@gmail.com>
2024-03-20 13:35:02 +0100

    dMRI_SQUAD sorting out

--- dMRI/dMRI_SQUAD_last_bit_only.py
+++ dMRI/dMRI_SQUAD_last_bit_only.py
@@ -1,7 +1,6 @@
-# Routine for running SQUAD in study
-# and saving to QC_SQUAD.tsv file
+## Routine for putting a SQUAD output (group_db.json) into a QC_SQUAD.tsv file
+## Decide QC pass_fail from EddyQC Traffic Light Criteria
 ####################################################
-## Decide QC pass_fail from Traffic Light Criteria
 
 import os, shutil, json
 import json
@@ -9,13 +8,16 @@ import numpy as np
 import pandas as pd
 
 # Define derivatives folder
-derivatives = '/data/1TSSD/NENAH_BIDS/derivatives/dMRI'
+studydir = '/data/1TSSD/NENAH_BIDS'
+studydir = '/Users/fi2313le/Research/Projects/UoS_-_HIE_NENAH-school-age/Data/NENAH_BIDS'
+derivatives = studydir + '/derivatives/dMRI'
 squadlistfile = os.path.join(derivatives,f'sub-NENAHGRP/qc/squad_quad_folders_exclNENAHC041.txt')
 squadfolder = os.path.join(derivatives,f'sub-NENAHGRP/qc/squad_with_grouping_exclNENAHC041')
 
-# Read the included quadfolder
-with open(squadlistfile, 'r') as f:
-  quadfolders = f.read()
+# Put the included participant_id into a dataframe
+df0 = pd.read_csv(squadlistfile, sep=" ", header=None)
+df0 = df0.replace("../../sub","sub", regex=True).replace("/qc/eddy_quad","", regex=True)
+df0.columns = ['participant_id'] 
 
 # Read SQUAD output (GROUP JSON-file)
 with open(os.path.join(squadfolder,'group_db.json'), 'r') as f:
@@ -23,7 +25,7 @@ with open(os.path.join(squadfolder,'group_db.json'), 'r') as f:
 # and put into dataframes
 df1 = pd.DataFrame(squad["qc_motion"], columns=['qc_motion_abs',  'qc_motion_rel'], dtype = float)
 #df2 = pd.DataFrame(squad["qc_cnr"], columns=['qc_snr_b0',  'qc_cnr_b1000',  'qc_cnr_b2600'], dtype = float)
-df3 = pd.DataFrame(squad["qc_outliers"], columns=['qc_outliers_tot', 'qc_outliers_b1000', 'qc_outliers_b2600','qc_outliers_pe','qc_outliers_unknown_check_with_pdf'], dtype = float)
+df3 = pd.DataFrame(squad["qc_outliers"], columns=['qc_outliers_tot', 'qc_outliers_b1000', 'qc_outliers_b2600','qc_outliers_pe_dirAP','qc_outliers_pe_dirPA'], dtype = float)
 # and a final dataframe
 df =  pd.concat([df1, df3['qc_outliers_tot']], axis=1, join='outer')
 #df =  pd.concat([df1,df2, df3['qc_outliers_tot']], axis=1, join='outer')
@@ -35,16 +37,13 @@ dfqc.columns = df.columns
 # Traffic Light system that is the output from SQUAD (GREEN with 1 SD; MODERATE = YELLOW between 1-2 SD; SEVERE = RED above 2 SD).
 dfqc[abs( df.mean(axis=0) - df ) < 2 * df.std(axis=0) ] = 0.5
 dfqc[abs( df.mean(axis=0) - df ) < 1 * df.std(axis=0) ] = 1
-
-####################################################
-## Write to output file
-# Get sID and ssID:s from quadfolders
-sID_ssID = [s.replace("../../", "") for s in quadfolders]
-sID = [s.replace("qc/eddy_quad", "") for s in sID_ssID]
-df_sID_ssID = pd.DataFrame(sID, columns=["participant_id"])
+# and a column just listing the lowest of a qc values
+dfmin  = pd.DataFrame(dfqc.min(axis=1), columns = ["qc_min"])
+dfqc =  pd.concat([dfmin, dfqc], axis=1, join='outer')
 
 # rename the columns
-dfqc.rename(columns = {'qc_motion_abs':'qc_motion_abs_pass_fail',
+dfqc.rename(columns = {'qc_min':'qc_min_pass_fail',
+                       'qc_motion_abs':'qc_motion_abs_pass_fail',
                        'qc_motion_rel':'qc_motion_rel_pass_fail',
                        'qc_outliers_tot':'qc_outliers_tot_pass_fail'}, 
                        inplace = True)
@@ -57,7 +56,8 @@ dfqc.rename(columns = {'qc_motion_abs':'qc_motion_abs_pass_fail',
 #                       'qc_outliers_tot_pass_fail':'qc_outliers_tot_pass_fail'}, 
 #                       inplace = True)
 
-dfqc =  pd.concat([df_sID_ssID, dfqc], axis=1, join='outer')
+# include the participant_id
+dfqc =  pd.concat([df0, dfqc], axis=1, join='outer')
 dfqc = dfqc.sort_values( by = 'participant_id')
 # and write to output-file
 squadqctsv = os.path.join(squadfolder,'QC_SQUAD.tsv')
