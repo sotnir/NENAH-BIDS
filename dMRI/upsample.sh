66d03d29b36cf74c89790500d39db7adff679604
RioPhillips <riophillips@gmail.com>
2024-06-10 13:36:33 +0200

    Updating

--- dMRI/upsample.sh
+++ dMRI/upsample.sh
@@ -127,7 +127,7 @@ fi
 echo "Using optimal BET f-value of $optimal_bet for subject $sID"
 
 # create brain mask
-meanb1000_file="meanb1000_$upsampled_dwi.mif.gz"
+meanb1000_file="$subjectdir/meanb1000_$upsampled_dwi.mif.gz"
 mask_file="mask_$upsampled_dwi.mif.gz"
 temp_meanb1000="meanb1000tmp.nii.gz"
 
