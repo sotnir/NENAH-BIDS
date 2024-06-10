014203d9a0cfd19ef0417a2dc3d22c9e754cfa15
RioPhillips <riophillips@gmail.com>
2024-06-10 09:50:31 +0200

    Updating

--- dMRI/average_response.sh
+++ dMRI/average_response.sh
@@ -88,7 +88,7 @@ calculate_group_average() {
   local response_files=()
 
   for sID in "${subjects[@]}"; do
-    files=$(find "$studydir/derivatives/dMRI/sub-$sID/dwi" -path "*/response/${response}_${tissue}_dwi_preproc.txt")
+    files=$(find "$studydir/derivatives/dMRI/sub-${sID}/dwi" -path "*/response/${response}_${tissue}_dwi_preproc.txt")
     response_files+=($files)
   done
 
