e04faeeb75af9eb475f0e75af783495be5ab10f9
RioPhillips <riophillips@gmail.com>
2024-07-09 09:57:54 +0200

    Updating

--- dMRI/thalamic_connectome.sh
+++ dMRI/thalamic_connectome.sh
@@ -63,7 +63,7 @@ thalamus_lobes_image="${datadir}/anat/thalamus_lobes.mif"
 thomas_lut="${studydir}/code/NENAH-BIDS/label_names/thomas_lut_excluding_RBGA.txt"
 
 # default lobes params
-lobes_convert="${studydir}/code/NENAH-BIDS/label_names/fs2lobes_excluding_thalamus.txt"
+lobes_convert="${studydir}/code/NENAH-BIDS/label_names/fs2lobes_cingsep_convert_excl_thalamus.txt"
 lobes_labels="${MRTRIXHOME}/share/mrtrix3/labelconvert/fs2lobes_cingsep_labels.txt"
 aparc_aseg="${studydir}/derivatives/sMRI_fs-segmentation/sub-${sID}/mri/aparc+aseg.mgz"
 FS_LUT="${FREESURFER_HOME}/FreeSurferColorLUT.txt"
@@ -151,7 +151,7 @@ if [ ! -f $thalamus_lobes_image ]; then
     mrcalc $thalamus_image $output_lobes_parcels -add $thalamus_lobes_image_float
     echo ""
     echo "Converting thalamus_lobes.mif to mrview supported datatype (float --> integer)"
-    mrconvert -datatype "uint32" $thalamus_lobes_image_float $thalamus_image
+    mrconvert -datatype "uint32" $thalamus_lobes_image_float $thalamus_lobes_image
     echo ""
 else
     echo "Combined thalamus and lobes image already exists"
