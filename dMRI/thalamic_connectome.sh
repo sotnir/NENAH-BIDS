7244bcce9f9aafec7d7969901b8dc183760e9ebf
RioPhillips <riophillips@gmail.com>
2024-07-09 10:23:46 +0200

    Updating

--- dMRI/thalamic_connectome.sh
+++ dMRI/thalamic_connectome.sh
@@ -138,7 +138,7 @@ if [ ! -d "$thalamus_image_dir" ]; then
     mkdir -p "$thalamus_image_dir"
 fi
 
-if [ ! -f $thalamus_image ]; then 
+if [ ! -f $thalamus_image_float ]; then 
     echo "Combining left and right thalamus --> thalamus.mif in /sub-${sID}/anat/connectome"
     mrcalc $right_output_thalamus_parcels $left_output_thalamus_parcels -add $thalamus_image_float
     echo ""
