1270732b3068759b66cea8896c77c16029b8773f
RioPhillips <riophillips@gmail.com>
2024-10-19 12:43:28 +0200

    Update

--- analysis/mann_whitney_u/mannwhitneyu.py
+++ analysis/mann_whitney_u/mannwhitneyu.py
@@ -69,7 +69,7 @@ corrected_p_matrix.T[np.triu_indices(p, k=1)] = corrected_p_matrix[np.triu_indic
 
 
 output_dir = "/data/iridis/NENAH_BIDS/code/NENAH-BIDS/analysis/mann_whitney_u/outputs/"
-output_file = os.path.join(output_dir, "new_corrected_p_values_matrix.csv")
+output_file = os.path.join(output_dir, "new2_corrected_p_values_matrix.csv")
 
 # save matrix to file
 np.savetxt(output_file, corrected_p_matrix, delimiter=",")
@@ -89,11 +89,11 @@ for i in range(p):
             significant_connections.append((i, j))
 
 
-csv_file = os.path.join(output_dir, "new_connections.csv")
+csv_file = os.path.join(output_dir, "new2_connections.csv")
 
 with open(csv_file, 'w') as f:
     f.write("Region 1,Region 2\n")
     for conn in significant_connections:
         f.write(f"{conn[0]},{conn[1]}\n")
 
-print(f"Significant connections saved to /outputs/new_connections.csv")
+print(f"Significant connections saved to /outputs/new2_connections.csv")
