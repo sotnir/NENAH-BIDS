62e303c8b75ec4b43daf3323631a5238b07f78e7
RioPhillips <riophillips@gmail.com>
2025-04-08 19:33:03 +0200

    updating nbs

--- analysis/NBS/run_NBS_ttest.py
+++ analysis/NBS/run_NBS_ttest.py
@@ -31,6 +31,8 @@ def build_ui_struct(connectome_path, design_path, contrast, size_method, thresho
     UI.design.ui='{design_path}';
     UI.exchange.ui='';
     UI.matrices.ui='{connectome_path}';
+    UI.node_label.ui='';
+    UI.node_coor.ui='';
     """
 
 def run_matlab_script(script_text):
