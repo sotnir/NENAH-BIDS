320e5c1de928fbb1b97f55e9afc1b2e6c747682e
RioPhillips <riophillips@gmail.com>
2025-04-08 22:59:50 +0200

    nbs

--- analysis/NBS/run_NBS_ttest.py
+++ analysis/NBS/run_NBS_ttest.py
@@ -79,7 +79,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--connectome_path', type=str, required=False, default='/data/iridis/NENAH_BIDS/code/NENAH-BIDS/analysis/NBS/connectivity_matrices/thalamus_lobes_connectome/NENAH002.txt')
     parser.add_argument('--LUT', type=str, required=False, default='/data/iridis/NENAH_BIDS/code/NENAH-BIDS/label_names/fs-lobes_thomas-thalamic-nuclear-groups_LUT-mrtrix3.txt')
-    parser.add_argument('--threshold', nargs='*', type=float, default=['2.5', '2.6', '2.7', '2.8', '2.9', '3.0'])
+    parser.add_argument('--threshold', nargs='*', type=float, default=[2.5, 2.6, 2.7, 2.8, 2.9, 3.0])
     parser.add_argument('--contrast_vector', type=str, default='[0,-1]')
     parser.add_argument('--design_matrix', type=str, default='/data/iridis/NENAH_BIDS/code/NENAH-BIDS/analysis/NBS/design_matrices/design_matrix_ttest.txt')
     parser.add_argument('--output_dir', type=str, default='/data/iridis/NENAH_BIDS/code/NENAH-BIDS/analysis/NBS/results/thalamus_lobes_connectome')
