51364c39dfed41b5b8b94b532a66428507ef4ff7
RioPhillips <riophillips@gmail.com>
2025-04-26 21:16:05 +0200

    NBS

--- analysis/NBS/significant_edges_remapping.py
+++ analysis/NBS/significant_edges_remapping.py
@@ -28,7 +28,7 @@ def read_lut(lut_path):
 # update these paths accordingly:
 studydir = os.getcwd()
 lut_path = os.path.join(studydir, "code", "NENAH-BIDS", "label_names", "fs-ctx_thomas-thalamic-nuclear-groups_LUT-mrtrix3.txt")
-output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "results", "fs-ctx_thomas_nuclear_groups_connectome", "2-8_threshold_thalamus_lobes_significant_connections.txt")
+output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "results", "fs-ctx_thomas_nuclear_groups_connectome", "2-5_threshold_thalamus_lobes_significant_connections.txt")
 
 # list of significant edges: (source index, target index, test statistic)
 # these are obtained by running this script in the MATLAB terminal after running the NBS:
@@ -42,13 +42,47 @@ output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "r
 # todo: write script to extract edges from matlab
 
 edges = [
+    (5, 9, 2.75),
+    (9, 15, 3.11),
+    (16, 22, 2.72),
+    (9, 24, 2.64),
+    (22, 24, 2.70),
+    (12, 34, 2.81),
+    (13, 34, 2.88),
+    (23, 34, 2.75),
+    (27, 34, 2.95),
+    (16, 35, 2.86),
+    (21, 35, 2.54),
+    (23, 35, 2.61),
+    (27, 36, 2.59),
+    (21, 46, 2.80),
+    (16, 48, 2.61),
+    (48, 50, 2.67),
+    (26, 53, 2.72),
+    (53, 54, 2.80),
+    (37, 56, 2.78),
+    (50, 56, 2.73),
+    (36, 57, 2.72),
+    (52, 59, 2.71),
+    (36, 60, 2.69),
+    (59, 60, 2.77),
+    (37, 61, 2.78),
+    (59, 61, 2.71),
+    (46, 68, 2.57),
     (53, 68, 2.82),
+    (54, 68, 2.65),
     (57, 68, 3.02),
+    (61, 68, 2.63),
+    (27, 70, 4.03),
+    (35, 70, 2.58),
+    (65, 73, 2.55),
     (68, 73, 2.95),
     (36, 74, 2.99),
     (61, 74, 3.76),
     (68, 74, 2.88),
+    (26, 75, 2.66),
     (68, 75, 3.30),
+    (36, 76, 2.59),
     (68, 76, 4.05),
 ]
 
@@ -64,6 +98,7 @@ for src, tgt, stat in edges:
     results.append(line)
 
 # --- Save the results to output file ---
+
 with open(output_path, "w") as out_file:
     out_file.write("\n".join(results))
 
