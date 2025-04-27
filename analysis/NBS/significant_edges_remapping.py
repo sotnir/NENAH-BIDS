2540bb1870077a7fdca5e45a1b2ed04ad30a2d60
RioPhillips <riophillips@gmail.com>
2025-04-27 20:38:51 +0200

    NBS

--- analysis/NBS/significant_edges_remapping.py
+++ analysis/NBS/significant_edges_remapping.py
@@ -27,8 +27,8 @@ def read_lut(lut_path):
 
 # update these paths accordingly:
 studydir = os.getcwd()
-lut_path = os.path.join(studydir, "code", "NENAH-BIDS", "label_names", "fs_thomas-thalamic_LUT-mrtrix3.txt")
-output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "results", "fs-thomas_whole_brain_connectome", "2-5_fs-thomas_whole_brain_connectome_significant_connections.txt")
+lut_path = os.path.join(studydir, "code", "NENAH-BIDS", "label_names", "fs-lobes_thomas-thalamic-nuclear-groups_LUT-mrtrix3.txt")
+output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "results", "fs-thomas_whole_brain_connectome", "2-5_fs-thomas_thalamus_lobes_2_significant_connections.txt")
 
 # list of significant edges: (source index, target index, test statistic)
 # these are obtained by running this script in the MATLAB terminal after running the NBS:
@@ -42,74 +42,19 @@ output_path = os.path.join(studydir, "code", "NENAH-BIDS", "analysis", "NBS", "r
 # todo: write script to extract edges from matlab
 
 edges = [
-    (5, 9, 2.51),
-    (16, 22, 2.70),
-    (9, 24, 2.68),
-    (22, 24, 2.70),
-    (12, 34, 2.52),
-    (13, 34, 3.38),
-    (16, 35, 2.86),
-    (21, 35, 2.54),
-    (23, 35, 2.61),
-    (27, 36, 2.58),
-    (9, 39, 2.70),
-    (24, 39, 2.55),
-    (16, 48, 2.61),
-    (48, 50, 2.67),
-    (26, 53, 2.72),
-    (53, 54, 2.80),
-    (50, 56, 2.73),
-    (36, 57, 2.77),
-    (47, 59, 3.07),
-    (52, 59, 2.70),
-    (36, 60, 2.59),
-    (59, 60, 2.77),
-    (37, 61, 2.78),
-    (59, 61, 2.73),
-    (46, 65, 2.80),
-    (12, 68, 2.55),
-    (53, 68, 2.76),
-    (57, 68, 2.60),
-    (20, 71, 2.86),
-    (34, 71, 2.58),
-    (27, 72, 2.92),
-    (34, 72, 3.11),
-    (27, 73, 2.72),
-    (9, 74, 3.07),
-    (71, 74, 2.60),
-    (68, 76, 2.93),
-    (72, 76, 2.60),
-    (44, 77, 2.67),
-    (36, 78, 2.50),
-    (61, 78, 3.16),
-    (59, 79, 2.90),
-    (61, 79, 2.92),
-    (68, 80, 2.92),
-    (77, 80, 3.16),
-    (78, 80, 3.90),
-    (79, 80, 3.11),
-    (68, 82, 3.64),
-    (78, 82, 3.19),
-    (18, 83, 2.52),
-    (31, 83, 2.53),
-    (71, 83, 3.14),
-    (27, 84, 3.54),
-    (35, 84, 2.93),
-    (65, 87, 2.55),
-    (73, 87, 2.61),
-    (77, 87, 3.58),
-    (13, 88, 2.72),
-    (36, 88, 2.95),
-    (61, 88, 3.56),
-    (26, 89, 2.75),
-    (36, 89, 2.67),
-    (39, 89, 3.02),
-    (77, 89, 2.63),
-    (36, 90, 2.57),
-    (59, 90, 2.54),
-    (68, 90, 2.78),
-    (77, 90, 2.80),
-    (79, 90, 2.74)
+    (1, 5, 3.34),
+    (2, 5, 3.62),
+    (1, 10, 3.09),
+    (10, 14, 4.02),
+    (1, 16, 2.57),
+    (7, 16, 2.59),
+    (7, 18, 2.61),
+    (8, 19, 3.03),
+    (8, 22, 3.74),
+    (1, 23, 3.25),
+    (8, 24, 3.25),
+    (14, 24, 3.68),
+    (22, 24, 2.78),
 ]
 
 # --- Process LUT and map edges ---
